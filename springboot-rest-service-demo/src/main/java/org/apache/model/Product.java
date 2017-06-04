a7b637241a0914680754902819c52ef991ef7cec
ANSHUL-ASUS\anshu <anshulrohilla.ac@gmail.com>
2017-06-04 14:10:00 -0700

    Refactoring repository structure

---
+++ springboot-rest-service-demo/src/main/java/org/apache/model/Product.java
@@ -0,0 +1,45 @@
+package org.apache.model;
+
+public class Product {
+
+	public Product(){
+		
+	}
+	
+	public Product(int productId, String name, int weight) {
+		super();
+		this.productId = productId;
+		this.name = name;
+		this.weight = weight;
+	}
+
+	private int productId;
+
+	public int getProductId() {
+		return productId;
+	}
+
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getWeight() {
+		return weight;
+	}
+
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+	private String name;
+
+    private int weight;
+}
