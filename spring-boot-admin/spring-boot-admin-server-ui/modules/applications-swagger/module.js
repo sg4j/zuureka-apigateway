9b76a87a00874fac22e504e488540ecce8eb2e13
ANSHUL-ASUS\anshu <anshulrohilla.ac@gmail.com>
2017-06-06 20:14:15 -0700

    Adding swagger tab in UI

---
+++ spring-boot-admin/spring-boot-admin-server-ui/modules/applications-swagger/module.js
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+'use strict';
+
+var angular = require('angular');
+
+var module = angular.module('sba-applications-swagger', ['sba-applications']);
+global.sbaModules.push(module.name);
+
+require('./css/module.css');
+
+module.component('swaggerComp', require('./components/swaggerComp.js'));
+module.service('swaggerService', require('./services/swaggerService.js'));
+module.controller('swaggerCtrl', require('./controllers/swaggerCtrl.js'));
+
+module.config(function ($stateProvider) {
+  $stateProvider.state('applications.swagger', {
+    url: '/swagger',
+    templateUrl: 'applications-swagger/views/swagger.html',
+    controller: 'swaggerCtrl'
+  });
+});
+
+module.run(function (ApplicationViews, $sce, $http) {
+  ApplicationViews.register({
+    order: 250,
+    title: $sce.trustAsHtml('<i class="fa fa-file-text-o fa-fw"></i>Swagger'),
+    state: 'applications.swagger',
+    show: function (application) {
+    	return true;/*
+      return $http.head('api/applications/' + application.id + '/swagger').then(function () {
+        return true;
+      }).catch(function () {
+        return false;
+      });
+    */}
+  });
+});
+
