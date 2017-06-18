049c12c9ae468ba4fd205a1ec8e270bb59b566ef
sg4j <sourabh707@yahoo.com>
2017-06-18 10:45:12 -0400

    Adding new endpoints to RegisteredServices API

--- zuureka-apigateway/src/main/java/org/apache/zuureka/gateway/controller/RegisteredServicesController.java
+++ zuureka-apigateway/src/main/java/org/apache/zuureka/gateway/controller/RegisteredServicesController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import com.netflix.discovery.shared.Application;
 
@@ -27,8 +29,29 @@ public class RegisteredServicesController {
 	@RequestMapping(value = "/", produces = "application/json")
 
 	public @ResponseBody List<Application> getListOfRegisteredServices() {
-		System.out.println("Came here in getListOfRegisteredServices");
 		return eurekaClient.getApplications().getRegisteredApplications();
 	}
 
+	/**
+	 * Returns a JSON with list of Instances of a particular service
+	 * 
+	 * @return
+	 */
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/{serviceName}", produces = "application/json")
+	public @ResponseBody List<InstanceInfo> getServiceInstances(@PathVariable(value="serviceName") String serviceName) {
+		return eurekaClient.getApplication(serviceName).getInstances();
+	}
+	
+	/**
+	 * Returns a JSON with Instance details of a particular service
+	 * 
+	 * @return
+	 */
+	@CrossOrigin(origins = "http://localhost:8080")
+	@RequestMapping(value = "/{serviceName}/instance/{instanceId}", produces = "application/json")
+	public @ResponseBody InstanceInfo getServiceInstanceDetails(@PathVariable(value="serviceName") String serviceName
+															   ,@PathVariable(value="instanceId") String instanceId) {
+		return eurekaClient.getApplication(serviceName).getByInstanceId(instanceId);
+	}	
 }
