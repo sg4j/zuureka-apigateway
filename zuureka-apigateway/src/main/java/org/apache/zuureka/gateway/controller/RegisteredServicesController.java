f1208355e675d38c31cc3f9ba12e5219b9b51657
sg4j <sourabh707@yahoo.com>
2017-06-17 21:46:05 -0400

    Adding a REST API endpint for getting service details

---
+++ zuureka-apigateway/src/main/java/org/apache/zuureka/gateway/controller/RegisteredServicesController.java
@@ -0,0 +1,30 @@
+package org.apache.zuureka.gateway.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.netflix.discovery.EurekaClient;
+import com.netflix.discovery.shared.Application;
+
+@Controller
+@RequestMapping("/zuureka/services")
+public class RegisteredServicesController {
+	
+	@Autowired
+	EurekaClient eurekaClient;
+
+		/**
+		 * Returns a JSON with list of service names
+		 * @return
+		 */
+		@RequestMapping("/")
+		public @ResponseBody List<Application> getListOfRegisteredServices() {
+			System.out.println("Came here in getListOfRegisteredServices");
+			return eurekaClient.getApplications().getRegisteredApplications();
+		}
+
+}
