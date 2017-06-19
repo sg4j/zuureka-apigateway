858828278375c808cf0cbcc4c49917a68e07233e
ANSHUL-ASUS\anshu <anshulrohilla.ac@gmail.com>
2017-06-18 22:50:12 -0700

    Adding service instance detail page

--- ZuurekaDashboard/src/main/resources/static/js/zuurekaDashboard.js
+++ ZuurekaDashboard/src/main/resources/static/js/zuurekaDashboard.js
@@ -1,11 +1,16 @@
 var app = angular.module('zuurekaDashboardApp', []);
 
-app.controller('zuurekaDashboardAppController', function($scope, $location,$http, $anchorScroll) {
+app.controller('zuurekaDashboardAppController', function($scope, $location,
+		$http, $anchorScroll) {
 
 	$scope.zuurekaGatewayPath = 'http://localhost:8761';
+	$scope.homePageVisibility = true;
+	$scope.serviceDetailsVisibility = false;
+	$scope.currentService = '';
+	$scope.currentServiceInstance = '';
+	$scope.currentServiceDetailTab = 'Overview';
 
-	if ($location.port() == '8761') 
-	{
+	if ($location.port() == '8761') {
 		$scope.zuurekaGatewayPath = '';
 	}
 
@@ -13,5 +18,44 @@ app.controller('zuurekaDashboardAppController', function($scope, $location,$http
 			function(response) {
 				$scope.servicesList = response.data;
 			});
+
+	$scope.$watch('serviceDetailsVisibility',
+			function() {
+				if (!$scope.serviceDetailsVisibility) {
+					return;
+				}
+			});
+	
+	$scope.showPage = function(pageNumber) {
+		$scope.homePageVisibility = false;
+		$scope.serviceDetailsVisibility = false;
+
+		if (pageNumber == 0) {
+			$scope.homePageVisibility = true;
+		} else if (pageNumber == 1) {
+			$scope.serviceDetailsVisibility = true;
+		} else {
+			$scope.homePageVisibility = true;
+		}
+	}
+
+	$scope.showServiceDetails = function(serviceSelected, instanceSelected) {
+		$scope.currentService = serviceSelected;
+		$scope.currentServiceInstance = instanceSelected;
+		$scope.showPage(1);
+	}
+	
+	$scope.isServiceDetailsTabSelected = function(currentServiceDetailsTabSelected){
+		if($scope.currentServiceDetailTab == currentServiceDetailsTabSelected) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	$scope.showServiceDetailsTab = function(currentServiceDetailsTabSelected)
+	{
+		$scope.currentServiceDetailTab = currentServiceDetailsTabSelected;
+	}
 
 });
